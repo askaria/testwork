428522aa0783f90ede8f36bef33ddb8b30baeb8e
Анастасия <askaria.1990@gmail.com>
2024-09-25 22:40:20 +0500

    Мелкие правки

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
-// lists
-const orderedLists = document.querySelectorAll('ol[start]');
-orderedLists.forEach((ol) => {
-    const start = ol.getAttribute('start');
-    if (start) {
-        ol.style.setProperty('--start', start);
-    }
-});
-
-// // splide 
 document.addEventListener('DOMContentLoaded', function () {
-    const sliders = document.querySelectorAll('.splide');
-    sliders.forEach(function (slider) {
-        new Splide(slider, {
-            type: 'loop',
-            gap: 20,
-            autoWidth: true,
-            pagination: false,
-            arrows: false,
-            autoplay: true,
-            interval: 3000,
-            drag: true,
-            wheel: true,
-            releaseWheel: true,
-            wheelSleep: 500,
-        }).mount(); // Инициализация слайдера
+    // lists
+    const orderedLists = document.querySelectorAll('ol[start]');
+    orderedLists.forEach((ol) => {
+        const start = ol.getAttribute('start');
+        if (start) {
+            ol.style.setProperty('--start', start);
+        }
     });
-});
 
+    // // splide 
+    const sliders = document.querySelectorAll('.splide');
+    if (sliders.length) {
+        sliders.forEach(function (slider) {
+            new Splide(slider, {
+                type: 'loop',
+                gap: 20,
+                autoWidth: true,
+                pagination: false,
+                arrows: false,
+                autoplay: true,
+                interval: 3000,
+                drag: true,
+                wheel: true,
+                releaseWheel: true,
+                wheelSleep: 500,
+            }).mount(); // Инициализация слайдера
+        });
+    }
 
-// glightbox
-document.addEventListener('DOMContentLoaded', function () {
+    // glightbox
     const glightboxElements = document.querySelectorAll('.glightbox');
     if (glightboxElements.length) {
         const glightbox = GLightbox({
@@ -42,48 +41,59 @@ document.addEventListener('DOMContentLoaded', function () {
             zoomable: false
         });
     }
-});
 
-/*
- * Mobile menu
- */
+    /*
+     * Mobile menu
+     */
 
-var menuOverlay = document.querySelector('.menu-overlay');
+    const menuOverlay = document.querySelector('.menu-overlay');
+    const body = document.body;
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
 
-function mobileMenu() {
-    var screenWidth = window.innerWidth;
-    if (screenWidth <= 980) {
-        var menuItems = document.querySelectorAll('.primary-menu .menu-item-has-children');
-        menuItems.forEach(function (menuItem) {
-            if (!menuItem.querySelector('.toggle')) {
-                var toggleDiv = document.createElement('div');
-                toggleDiv.classList.add('toggle');
-                menuItem.appendChild(toggleDiv);
-            }
-        });
+    function mobileMenu() {
+        const screenWidth = window.innerWidth;
+        if (screenWidth <= 980) {
+            const primaryMenu = document.querySelector('.primary-menu');
+            const menuItems = document.querySelectorAll('.primary-menu .menu-item.has-children');
 
-        document.querySelector('.primary-menu').addEventListener('click', function (event) {
-            if (event.target.classList.contains('toggle')) {
-                var subMenu = event.target.nextElementSibling;
-                if (subMenu && subMenu.classList.contains('sub-menu')) {
-                    subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+            menuItems.forEach(function (menuItem) {
+                if (!menuItem.querySelector('.toggle')) {
+                    const toggleDiv = document.createElement('div');
+                    toggleDiv.classList.add('toggle');
+                    menuItem.appendChild(toggleDiv);
                 }
-            }
-        });
+            });
+
+            // Делегирование событий для меню
+            primaryMenu.addEventListener('click', function (event) {
+                if (event.target.classList.contains('toggle')) {
+                    const subMenu = event.target.previousElementSibling;
+                    if (subMenu && subMenu.classList.contains('sub-menu')) {
+                        subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+                    }
+                }
+            });
+        }
     }
-}
-mobileMenu();
+    mobileMenu();
 
-document.getElementById('mobile-menu-toggle').addEventListener('change', function () {
-    menuOverlay.classList.toggle('active');
-    document.body.classList.toggle('mobile-menu-active');
-});
-
-document.querySelectorAll('.menu-overlay, .primary-menu .cross').forEach(function (element) {
-    element.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.getElementById('mobile-menu-toggle').checked = false;
-        menuOverlay.classList.remove('active');
-        document.body.classList.remove('mobile-menu-active');
+    // переключатель меню
+    mobileMenuToggle.addEventListener('change', function () {
+        menuOverlay.classList.toggle('active');
+        body.classList.toggle('mobile-menu-active');
     });
+
+    // Закрытие мобильного меню по нажатию на overlay или крестик
+    document.querySelectorAll('.menu-overlay, .primary-menu .cross').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            mobileMenuToggle.checked = false;
+            menuOverlay.classList.remove('active');
+            body.classList.remove('mobile-menu-active');
+        });
+    });
+
+    // ресайз
+    window.addEventListener('resize', mobileMenu);
+
 });
